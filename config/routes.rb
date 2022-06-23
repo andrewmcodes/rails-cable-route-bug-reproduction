ccf620b93c2ccab6615a304ccd0d5602c9f54010
Andrew Mason <andrewmcodes@protonmail.com>
2022-06-23 05:43:15 -0700

    chore: add bug reproduction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/cable-hyphenated-slug", to: "pages#index"
+  get "/cable_underscored_slug", to: "pages#index"
+  get "/cable_mixed-slug", to: "pages#index"
 end
---
+++ test/system/pages_test.rb
@@ -0,0 +1,32 @@
+require "application_system_test_case"
+
+class PagesTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit root_url
+
+    assert_no_selector "pre", text: "Page not found"
+    assert_selector "h1", text: "PagesController#index"
+  end
+
+  # Broken test to verify the behavior of the application.
+  test "visiting the hyphenated path prefixed with cable" do
+    visit cable_hyphenated_slug_path
+
+    assert_no_selector "pre", text: "Page not found"
+    assert_selector "h1", text: "PagesController#index"
+  end
+
+  test "visiting the underscored path prefixed with cable" do
+    visit cable_underscored_slug_path
+
+    assert_no_selector "pre", text: "Page not found"
+    assert_selector "h1", text: "PagesController#index"
+  end
+
+  test "visiting the underscored then hyphenated path prefixed with cable" do
+    visit cable_mixed_slug_path
+
+    assert_no_selector "pre", text: "Page not found"
+    assert_selector "h1", text: "PagesController#index"
+  end
+end
