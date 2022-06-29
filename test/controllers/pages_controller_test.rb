97ac30b539658c8dd1380c9e805c8b57179403f9
Andrew Mason <andrewmcodes@protonmail.com>
2022-06-29 03:59:18 -0700

    test: add controller tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "route prefixed with 'cable-' should get index" do
+    get "/cable-hyphenated-slug"
+    assert response.ok?
+  end
+
+  test "route prefixed with 'cable_' should get index" do
+    get "/cable_underscored_slug"
+    assert response.ok?
+  end
 end
